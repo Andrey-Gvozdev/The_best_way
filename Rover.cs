ee102f560b02f3d74e65404d783917c4d1f2fe28
Andrey-Gvozdev <andrey.gvozdeu@gmail.com>
2021-04-29 01:26:24 +0300

    Added main algorithm and fixed TakeActivePoint method

--- Rover.cs
+++ Rover.cs
@@ -3,7 +3,6 @@ using System.Collections.ObjectModel;
 
 public class Rover {
 
-    
     public static void CalculateRoverPath(int[,] map) {
         var openList = new Collection<Point>();
         var closedList = new Collection<Point>();
@@ -27,42 +26,56 @@ public class Rover {
         };
 
         neighborList.Add(startPoint);
+        openList.Add(startPoint);
 
         while (true)
         {
-            Point activePoint = TakeActivePoint(goalPoint, neighborList);
+            Point activePoint = TakeActivePoint(goalPoint, openList);
             
             if (CalcHeuristic(activePoint, goalPoint) == 1)
             {
                 Point pathPoint = goalPoint;
-                do
+                pathPoint.PrevPoint = activePoint;
+                pathPoint.FuelFromStart = activePoint.FuelFromStart + Math.Abs(activePoint.Height - map[map.GetUpperBound(0), map.GetUpperBound(1)]) + 1;
+                while (pathPoint != null)
                 {
                     pathList.Add(pathPoint);
                     pathPoint = pathPoint.PrevPoint;
                 }
-                while (pathPoint != startPoint);
                 //output in .txt
 
                 break;
             }
 
             closedList.Add(activePoint);
+            openList.Remove(activePoint);
 
-            //AddingNeighborPoints(currentPoint, map, openList, neighborList);
+            AddingNeighborPoints(activePoint, map, neighborList);
 
             foreach (var challengerPoint in neighborList)
             {
-                foreach (var openListPoint in openList)
+                int closedCounter = 0;
+                int openCounter = 0;
+                foreach (var closedListPoint in closedList)
                 {
-                    if (challengerPoint.CoordPoint[0] == openListPoint.CoordPoint[0] && challengerPoint.CoordPoint[1] == openListPoint.CoordPoint[1])
+                    if(challengerPoint.CoordPoint[0] == closedListPoint.CoordPoint[0] && challengerPoint.CoordPoint[1] == closedListPoint.CoordPoint[1])
                     {
-                        if (challengerPoint.FuelFromStart < openListPoint.FuelFromStart)
-                        {
-
-                        }
+                        closedCounter++;
                     }
                 }
-                //openList.Add();
+                if (closedCounter != 0)
+                    continue;
+                foreach (var openListPoint in openList)
+                {
+                    if (challengerPoint.CoordPoint[0] == openListPoint.CoordPoint[0] && challengerPoint.CoordPoint[1] == openListPoint.CoordPoint[1] && challengerPoint.FuelFromStart < openListPoint.FuelFromStart)
+                    {
+                        openListPoint.PrevPoint = challengerPoint.PrevPoint;
+                        openListPoint.FuelFromStart = challengerPoint.FuelFromStart;
+                        openCounter++;
+                    }
+                }
+                if (openCounter == 0)
+                    openList.Add(challengerPoint);
             }
         }
     }
@@ -123,16 +136,14 @@ public class Rover {
         }
     }
 
-    public static Point TakeActivePoint(Point goalPoint, Collection<Point> neighborList)
+    public static Point TakeActivePoint(Point goalPoint, Collection<Point> openList)
     {
-        Point activePoint = neighborList[0];
+        Point activePoint = openList[0];
         
-        foreach (var point in neighborList)
+        foreach (var point in openList)
         {
             if (CalcHeuristic(point, goalPoint) + point.FuelFromStart < CalcHeuristic(activePoint, goalPoint) + activePoint.FuelFromStart)
-            {
                 activePoint = point;
-            }
         }
 
         return activePoint;
