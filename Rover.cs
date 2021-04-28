341e1a6adedb253eafbe4e6716037adffd5c1e10
Andrey-Gvozdev <andrey.gvozdeu@gmail.com>
2021-04-28 19:02:10 +0300

    Creating open and closed liststs

--- Rover.cs
+++ Rover.cs
@@ -1,6 +1,17 @@
+using System;
+using System.Collections.ObjectModel;
+
 public class Rover {
 
+    
     public static void CalculateRoverPath(int[,] map) {
+        var openList = new Collection<int[]>();
+        var closedList = new Collection<int[]>();
+
+        //Point info { active i, active j, fuel from start point, prev i, prev j}
+        int[] startPoint = { 0, 0, map[0, 0] };
+        int[] goalPoint = { map.GetUpperBound(0), map.GetUpperBound(1), map[map.GetUpperBound(0), map.GetUpperBound(1)] };
+
     }
 
 }
